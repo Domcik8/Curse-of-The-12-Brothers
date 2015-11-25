b29f4b7ba9d8bed16a9fabdb6a630179862ca2c8
Migle224 <mpucetaite@gmail.com>
2015-11-25 12:11:18 +0200

    Shadow changes

--- Assets/Scripts/GirlsScripts/Shadow.cs
+++ Assets/Scripts/GirlsScripts/Shadow.cs
@@ -12,27 +12,36 @@ public class Shadow : MonoBehaviour {
     public float shadowH = 7;
     float distance = 0;
 
+    bool girlIsDead = false;
+
 
     // Use this for initialization
-    void Start () {
-         ground = GameObject.FindGameObjectWithTag("Ground");
-      
-	}
+    void Start() {
+        ground = GameObject.FindGameObjectWithTag("Ground");
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        if ((player != null) && (!girlIsDead))
         {
             distance = player.transform.position.y - gameObject.transform.position.y;
-            gameObject.transform.position = new Vector2(player.transform.position.x , ground.transform.position.y + 0.5f);
+            gameObject.transform.position = new Vector2(player.transform.position.x, ground.transform.position.y + 0.5f);
             gameObject.transform.localScale = new Vector2(shadowW / distance, shadowH / distance);
         }
     }
 
     public void ShadowDeath()
     {
+
+        Invoke("ChangeScale", 0.7f);
+    }
+
+    void ChangeScale()
+    {
         gameObject.transform.localScale = new Vector2(shadowW / distance * 2, shadowH / distance);
+        girlIsDead = true;
 
     }
 }
